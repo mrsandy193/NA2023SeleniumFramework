af2a742c3574b8eda98bbb4322d8cb3d846df217
Sandeep <sk.kh193@gmail.com>
2023-03-19 15:29:10 +0530

    Added order Page

---
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class OrderPage {
+	By loc = By.id("");
+	
+	public void getOrder()
+	{
+		System.out.println("Get Order");
+	}
+}
