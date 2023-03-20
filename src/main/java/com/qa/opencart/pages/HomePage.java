0df5051eebd45e1dfc0df3f3a5948ac929dc0536
mrsandy193 <43105393+mrsandy193@users.noreply.github.com>
2023-03-20 20:41:49 +0530

    Create HomePage.java
    Added HomePage

---
+++ src/main/java/com/qa/opencart/pages/HomePage.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class HomePage {
+	By HomePageLogo = By.id("logo");
+  
+  public void verifyHomePageLogo()
+  {
+    System.out.println("Home Page Logo Verification");
+  }
+}
